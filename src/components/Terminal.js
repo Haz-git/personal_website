2461ff5bed16113929f014dacd571626c7877114
Haz-git <zhouharry1@gmail.com>
2020-09-07 12:35:34 -0700

    Final Edits To Terminal

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -13,7 +13,66 @@ const Terminal = () => {
                     </div>
                 </div>
                 <div className='terminal-body'>
-                    <div className='terminal-text'></div>
+                    <div className='terminal-text'>
+                        <div>
+                            <h4 className='comment'>//const Harry = new Developer();</h4>
+                        </div>
+                        <br/>
+                        <div className='terminal-text-block'>
+                            <h4>
+                                > Harry.currentLocation;
+                                <br />
+                                "San Diego, CA"
+                            </h4>
+                        </div>
+                        <br />
+                        <div className='terminal-text-block'>
+                            <h4>
+                                > Harry.personalInterests;
+                                <br />
+                                ["Art", "PowerLifting", "Traveling", "Cooking"]
+                            </h4>
+                        </div>
+                        <br />
+                        <div className='terminal-text-block'>
+                            <h4>
+                                > Harry.education;
+                                <br />
+                                ["B.S Human Biology - University Of California, San Diego", "(In Progress) M.S Biological Sciences (Molecular/Micro) - University of California, San Diego"]
+                            </h4>
+                        </div>
+                        <br />
+                        <div className='terminal-text-block'>
+                            <h4>
+                                > Harry.resumeInfo;
+                                <br />
+                                Undefined
+                            </h4>
+                        </div>
+                        <br />
+                        <div className='terminal-text-block'>
+                            <h4>
+                                > Harry.contactInfo;
+                                <br />
+                                Undefined
+                            </h4>
+                        </div>
+                        <br />
+                        <div className='terminal-text-block'>
+                            <h4>
+                                > Harry.skillList;
+                                <br />
+                                ["HTML5", "CSS3", "Javascript", "Python", "Java", "React", "Redux"]
+                            </h4>
+                        </div>
+                        <br />
+                        <div className="terminal-text-block inline">
+                            <h4>
+                                > Harry.setNewInterview();
+                                <div className="blinker-square"></div>
+                            </h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
