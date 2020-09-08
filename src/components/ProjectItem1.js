e6d9d9ca43d0b8acc5371a0b40d17fd988e2b281
Haz-git <zhouharry1@gmail.com>
2020-09-08 12:11:29 -0700

    Made Website Mobile Responsive, Added More Styling To Project Cards.

--- src/components/ProjectItem1.js
+++ src/components/ProjectItem1.js
@@ -7,54 +7,58 @@ const ProjectItem1 = ({ projTitle, projImg }) => {
     const [title, setTitle] = useState(projTitle);
 
     return (
-        <div className="project-card-container">
-            <div className="project-card-header">
-                <h2>{title}</h2>
-            </div>
-            <div className="project-desc-container">
-                <div className="project-img">
-                    <Fade left>
-                        <img src={projImg}></img>
-                    </Fade>
+        <div className="project-card-wrapper">
+            <div className="project-card-container">
+                <div className="project-card-header">
+                    <h2>{title}</h2>
                 </div>
-                <div className="project-desc">
-                    <Fade right>
-                        <h4>Like most people, I primarily create webapps to make my daily life easier.<br/><br/> This project was built for timing the numerous activites and experiments I must keep track of during a day in my wet lab--Enter a note in the template provided, input a length of time, and organize many tasks at once.<br/><br/> As this was one of my first major projects using solely HTML5, CSS3, and JS, I learned much about event listeners, generating elements in the DOM, and general layout design!</h4>
-                    </Fade>
-                </div>
-            </div>
-            <div className="project-skill-labels">
-                <div className="skill-logo-container">
-                    <div className="skill-logo-html">
-                        <i class="fab fa-html5 fa-3x"></i>
+                <div className="project-desc-container">
+                    <div className="project-img">
+                        <Fade left>
+                            <img src={projImg}></img>
+                        </Fade>
+                    </div>
+                    <div className="project-desc">
+                        <Fade right>
+                            <h4>Like most people, I primarily create webapps to make my daily life easier.<br/><br/> This project was built for timing the numerous activites and experiments I must keep track of during a day in my wet lab--Enter a note in the template provided, input a length of time, and organize many tasks at once.<br/><br/> As this was one of my first major projects using solely HTML5, CSS3, and JS, I learned much about event listeners, generating elements in the DOM, and general layout design!</h4>
+                            <div className="project-skill-labels">
+                                <div className="skill-logo-container">
+                                    <div className="skill-logo-html">
+                                        <i class="fab fa-html5 fa-3x"></i>
+                                    </div>
+                                </div>
+                                <div className="skill-logo-container">
+                                    <div className="skill-logo-css">
+                                        <i class="fab fa-css3-alt fa-3x"></i>
+                                    </div>
+                                </div>
+                                <div className="skill-logo-container">
+                                    <div className="skill-logo-js">
+                                        <i class="fab fa-js fa-3x"></i>
+                                    </div>
+                                </div>
+                                {/* <div className="skill-logo-container">
+                                    <div className="skill-logo-react">
+                                        <i class="fab fa-react fa-3x"></i>
+                                    </div>
+                                </div> */}
+                            </div>
+                        </Fade>
                     </div>
                 </div>
-                <div className="skill-logo-container">
-                    <div className="skill-logo-css">
-                        <i class="fab fa-css3-alt fa-3x"></i>
+                <Fade bottom>
+                    <div className="project-buttons-container">
+                        <div className="project-button">
+                            <button className="ui primary button">Live Demo</button>
+                        </div>
+                        <div className="project-button">
+                            <button className="ui button">View Source</button>
+                        </div>
                     </div>
-                </div>
-                <div className="skill-logo-container">
-                    <div className="skill-logo-js">
-                        <i class="fab fa-js fa-3x"></i>
-                    </div>
-                </div>
-                {/* <div className="skill-logo-container">
-                    <div className="skill-logo-react">
-                        <i class="fab fa-react fa-3x"></i>
-                    </div>
-                </div> */}
+                </Fade>
             </div>
-            <div className="project-buttons-container">
-                <div className="project-button">
-                    <button className="ui primary button">Live Demo</button>
-                </div>
-                <div className="project-button">
-                    <button className="ui button">View Source</button>
-                </div>
-            </div>
-        </div>
-    )
+        </div>    
+    )   
 }
 
 export default ProjectItem1;
